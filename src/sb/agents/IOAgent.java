40cadd36a5dd480c4d03a62bf94dd81dd804fa50
Michaël SCHERER <michael.scherer@viacesi.fr>
2015-10-07 00:25:18 +0200

    New created Agents

---
+++ src/sb/agents/IOAgent.java
@@ -0,0 +1,31 @@
+package sb.agents;
+
+import jade.core.Agent;
+
+public class IOAgent extends Agent {
+
+	private String strAgrs[] = new String[20];
+
+	@Override
+	protected void setup() {
+		System.out.println("Agent INIT : " + getAID().getName());
+
+		Object args[] = getArguments();
+		if(args != null && args.length > 0) {
+			for (int i = 0; i < args.length; i++) {
+				strAgrs [i] = (String) args[i];
+			}
+		}
+		else {
+			doDelete();
+		}
+		// TODO Auto-generated method stub
+		super.setup();
+	}
+	
+	@Override
+	protected void takeDown() {
+		// TODO Auto-generated method stub
+		super.takeDown();
+	}
+}
---
+++ src/sb/agents/LuminosityAgent.java
@@ -0,0 +1,32 @@
+package sb.agents;
+
+import jade.core.Agent;
+
+public class LuminosityAgent extends Agent {
+
+	private String strAgrs[] = new String[20];
+
+	@Override
+	protected void setup() {
+		System.out.println("Agent INIT : " + getAID().getName());
+
+		Object args[] = getArguments();
+		if(args != null && args.length > 0) {
+			for (int i = 0; i < args.length; i++) {
+				strAgrs [i] = (String) args[i];
+			}
+		}
+		else {
+			doDelete();
+		}
+		// TODO Auto-generated method stub
+		super.setup();
+	}
+	
+	@Override
+	protected void takeDown() {
+		// TODO Auto-generated method stub
+		super.takeDown();
+	}
+
+}
